401bfd6b0338b3eb560c27eec38fccd8d1ebae4f
Jonas Kundra <jkundra@eisgroup.com>
2015-01-08 11:33:17 +0200

    Added Bookmark example

---
+++ iMacros/Bookmarks/ExampleLogin.js
@@ -0,0 +1,9 @@
+/**
+ * Bookmark example 
+ * Add this source to bookmark 'Location:' field.
+ */
+javascript: (function() {
+	var targetScript = "EIS/login/LoginAsQa.js";
+	content.document.getElementById("targetScript").setAttribute("value", targetScript);
+	window.location = 'imacros://run/?m=Proxy.js';
+}());
