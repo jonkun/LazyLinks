78049905f0c902aad43d992fd0544796131b432b
Jonas Kundra <jkundra@eisgroup.com>
2015-01-11 20:17:04 +0200

    Renamed import methods

--- iMacros/Bookmarks/ExampleLogin.js
+++ iMacros/Bookmarks/ExampleLogin.js
@@ -3,13 +3,13 @@
  *
  * Add this source to bookmark 'Location:' field.
  */
-
+var urlPrefix = 'file:///d:/exigen/src/LazyLinks/';
 javascript: (function() {
-	var targetScript = 'file:///d:/exigen/src/LazyLinks/Scripts/EIS/login/LoginAsQaExtended.js';
-	var targetElement = content.document.getElementById('targetScript');
+	var targetScript = urlPrefix + 'Scripts/EIS/Benefits/CertificatePolicy/DataGather/FillCertificatePolicyDetails.js';
+	var targetElement = content.document.getElementById('paramsBroker');
 	if (typeof(targetElement) == 'undefined' || targetElement === null) {
 		var input = content.document.createElement('input');
-		input.setAttribute('id', 'targetScript');
+		input.setAttribute('id', 'paramsBroker');
 		input.setAttribute('type', 'hidden');
 		input.setAttribute('value', targetScript);
 		content.document.body.appendChild(input);
