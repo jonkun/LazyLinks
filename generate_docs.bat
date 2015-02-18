32cf208c87dc2352b6cf7459dfd8fbf219761940
Jonas Kundra <jkundra@eisgroup.com>
2015-02-18 12:45:54 +0200

    Documentation updates

--- generate_docs.bat
+++ generate_docs.bat
@@ -1,6 +1,10 @@
 :: npm install -g jsdoc
 :: npm install ink-docstrap
 rm -R docs
-jsdoc -c ./conf.json ./../LazyLinks/Readme.md
+call jsdoc -c ./conf.json ./../LazyLinks/Readme.md
+
+cd docs
+mklink /j docs .\..\..\LazyLinks\docs
+mklink /j Greasemonkey .\..\..\LazyLinks\Greasemonkey
 
 :: docker -i iMacros -o docs
