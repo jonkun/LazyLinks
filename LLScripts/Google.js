b8f3a28455d4a2ebd4a26f8ebc962b4efe6141f9
Jonas Kundra <jonas@kundra.lt>
2015-02-28 18:58:42 +0200

    Example updates

--- LLScripts/Google.js
+++ LLScripts/Google.js
@@ -10,6 +10,12 @@ googlePage.searchBtn.click();
 googlePage.searchField.value('JS AGAIN');
 googlePage.searchBtn.click();
 
+
+
+
+
+
+
 /* Additional examples: */
 /* Example 1 */
 play('./Google.iim');
@@ -18,9 +24,9 @@ play('./Google.iim');
 playMacro("TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=", "value");
 
 /* Example 3 */
-var macros = ""
-macros += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=Test" + "\n";
-macros += "TAG POS=1 TYPE=BUTTON FORM=ID:tsf ATTR=NAME:btnG" + "\n";
-macros += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=again" + "\n";
-macros += "TAG POS=1 TYPE=BUTTON FORM=ID:tsf ATTR=NAME:btnG" + "\n";
-playMacro(macros);
+var iimScript = ""
+iimScript += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=Test" + "\n";
+iimScript += "TAG POS=1 TYPE=BUTTON FORM=ID:tsf ATTR=NAME:btnG" + "\n";
+iimScript += "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=again" + "\n";
+iimScript += "TAG POS=1 TYPE=BUTTON FORM=ID:tsf ATTR=NAME:btnG" + "\n";
+playMacro(iimScript);
