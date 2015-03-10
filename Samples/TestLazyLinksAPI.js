9d282870fb4be333c47a2f40b174adbe8ea96e42
Jonas Kundra <jonas@kundra.lt>
2015-03-10 12:48:55 +0200

    TestLLAPi updates

--- Samples/TestLazyLinksAPI.js
+++ Samples/TestLazyLinksAPI.js
@@ -28,33 +28,58 @@
 
 /**
  * Test page: http://demo.imacros.net/
- * 
+ *
  */
 
-config.macrosFolder;
-screenshot('c:\\src\\', 'testasd');
-
-goToUrl('http://');
-
 /**
- * Check LazyLinks API functions 
+ * Test scenario
+ * Navigate to www.google.com
+ * Enter text '' to search field and click Ok button
  */
 
 clear();
 
-goToUrl('http://wiki.imacros.net/SAVE_ELEMENT_SCREENSHOT');
+iimDisplay('Load extended iMacros');
 
-playMacro('TAG POS=1 TYPE=A ATTR=TXT:Command<SP>Reference');
+var predefinedMacros = {
+	/* Google page iMacros */
+	"searchField": "TAG POS=1 TYPE=INPUT:TEXT FORM=ID:tsf ATTR=ID:lst-ib CONTENT=",
+	"searchBtn": "TAG POS=1 TYPE=BUTTON FORM=ID:tsf ATTR=NAME:btnG"
+};
 
-wait(2)
+var googlePage = new LLMacros(predefinedMacros);
 
-back();
+goToUrl('www.google.com');
 
-pause('Please click Continue');
+
+googlePage.searchField.value('LazyLinks web automation tool');
+googlePage.searchBtn.click();
+
+wait(10, 'Waiting 10 seconds');
 
 refresh();
 
-goToUrl('http://jkundra:8080/ipb-app');
 
-screenshot('c:\\src\\', 'testasd');
+back();
 
+// screenshot('config.macrosFolder', 'Screenshoot_' + new Date());
+
+// goToUrl('http://');
+
+/**
+ * Check LazyLinks API functions
+ */
+
+
+
+// goToUrl('http://wiki.imacros.net/SAVE_ELEMENT_SCREENSHOT');
+
+// playMacro('TAG POS=1 TYPE=A ATTR=TXT:Command<SP>Reference');
+
+
+// pause('Please click Continue');
+
+
+// goToUrl('http://jkundra:8080/ipb-app');
+
+// screenshot('c:\\src\\', 'testasd');
