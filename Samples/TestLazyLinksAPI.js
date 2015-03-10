f96eb6efd6954a0960af08f34be6c5463d055c1e
Jonas Kundra <jonas@kundra.lt>
2015-03-10 11:33:38 +0200

    TestLazyAPI changes

--- Samples/TestLazyLinksAPI.js
+++ Samples/TestLazyLinksAPI.js
@@ -1,2 +1,39 @@
+/* Global functions list */
+// back
+// clear
+// fileDelete
+// getCookie
+// getUrlParam
+// goToUrl
+// id
+// include
+// load
+// loadResource
+// log
+// logError
+// logStyled
+// macros
+// openFile
+// pause
+// play
+// playMacro
+// readFile
+// refresh
+// saveAs
+// screenshot
+// setCookie
+// tab
+// wait
+// writeToFile
 
+/**
+ * Test page: http://demo.imacros.net/
+ * 
+ */
+
+config.macrosFolder;
 screenshot('c:\\src\\', 'testasd');
+
+goToUrl('http://');
+
+clear();
